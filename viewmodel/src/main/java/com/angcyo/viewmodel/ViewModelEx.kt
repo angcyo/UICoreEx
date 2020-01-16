c15c37f8d2840e1304762d6c4d7da5f6d93259da
angcyo <angcyo@126.com>
2020-01-16 23:46:51 +0800

    + view model library

---
+++ viewmodel/src/main/java/com/angcyo/viewmodel/ViewModelEx.kt
@@ -0,0 +1,46 @@
+package com.angcyo.viewmodel
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+/**
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/01/16
+ */
+
+/**单独获取[Fragment]对应的[ViewModelProvider]*/
+fun Fragment.of(factory: ViewModelProvider.Factory? = null) =
+    ViewModelProviders.of(this, factory)
+
+/**获取[Fragment]所在[FragmentActivity]中包含的[ViewModelProvider]*/
+fun Fragment.ofShare(factory: ViewModelProvider.Factory? = null) =
+    ViewModelProviders.of(this.requireActivity(), factory)
+
+/**单独获取[FragmentActivity]对应的[ViewModelProvider]*/
+fun FragmentActivity.of(factory: ViewModelProvider.Factory? = null) =
+    ViewModelProviders.of(this, factory)
+
+/**获取[ViewModelStore]中的[ViewModel], 默认的[key]是[DEFAULT_KEY + ":" + modelClass.getCanonicalName()]*/
+inline fun <reified VM : ViewModel> Fragment.viewModel(factory: ViewModelProvider.Factory? = null) =
+    of(factory).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> Fragment.viewModelShare(factory: ViewModelProvider.Factory? = null) =
+    ofShare(factory).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> FragmentActivity.viewModel(factory: ViewModelProvider.Factory? = null) =
+    of(factory).get(VM::class.java)
+
+/**缩短的方法名*/
+inline fun <reified VM : ViewModel> Fragment.vm(factory: ViewModelProvider.Factory? = null) =
+    viewModel<VM>(factory)
+
+inline fun <reified VM : ViewModel> Fragment.vmShare(factory: ViewModelProvider.Factory? = null) =
+    viewModelShare<VM>(factory)
+
+inline fun <reified VM : ViewModel> FragmentActivity.vm(factory: ViewModelProvider.Factory? = null) =
+    viewModel<VM>(factory)
