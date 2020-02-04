691407acdee330560bc5c873f6376063b0487a3d
angcyo <angcyo@126.com>
2020-02-04 15:56:30 +0800

    * fix viewmodel

--- viewmodel/src/main/java/com/angcyo/viewmodel/ViewModelEx.kt
+++ viewmodel/src/main/java/com/angcyo/viewmodel/ViewModelEx.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
 
 /**
  *
@@ -14,27 +15,10 @@ import androidx.lifecycle.ViewModelStore
  * @date 2020/01/16
  */
 
-/**单独获取[Fragment]对应的[ViewModelProvider]*/
-fun Fragment.of(factory: ViewModelProvider.Factory? = null) =
+//<editor-fold desc="返回ViewModelProvider">
+
+fun ViewModelStoreOwner.of(factory: ViewModelProvider.Factory? = null): ViewModelProvider =
     ViewModelProvider(this, factory ?: ViewModelProvider.NewInstanceFactory())
-
-/**获取[Fragment]所在[FragmentActivity]中包含的[ViewModelProvider]*/
-fun Fragment.ofa(factory: ViewModelProvider.Factory? = null) =
-    ViewModelProvider(this.requireActivity(), factory ?: ViewModelProvider.NewInstanceFactory())
-
-/**单独获取[FragmentActivity]对应的[ViewModelProvider]*/
-fun FragmentActivity.of(factory: ViewModelProvider.Factory? = null) =
-    ViewModelProvider(this, factory ?: ViewModelProvider.NewInstanceFactory())
-
-/**获取[ViewModelStore]中的[ViewModel], 默认的[key]是[DEFAULT_KEY + ":" + modelClass.getCanonicalName()]*/
-inline fun <reified VM : ViewModel> Fragment.vm(factory: ViewModelProvider.Factory? = null) =
-    of(factory).get(VM::class.java)
-
-inline fun <reified VM : ViewModel> Fragment.vma(factory: ViewModelProvider.Factory? = null) =
-    ofa(factory).get(VM::class.java)
-
-inline fun <reified VM : ViewModel> FragmentActivity.vm(factory: ViewModelProvider.Factory? = null) =
-    of(factory).get(VM::class.java)
 
 //自定义[ViewModelStore]
 
@@ -49,8 +33,38 @@ fun ViewModelStore.of(
     )
 }
 
+/**单独获取[Fragment]对应的[ViewModelProvider]*/
+fun Fragment.of(factory: ViewModelProvider.Factory? = null): ViewModelProvider =
+    ViewModelProvider(this, factory ?: ViewModelProvider.NewInstanceFactory())
+
+/**获取[Fragment]所在[FragmentActivity]中包含的[ViewModelProvider]*/
+fun Fragment.ofa(factory: ViewModelProvider.Factory? = null): ViewModelProvider =
+    ViewModelProvider(this.requireActivity(), factory ?: ViewModelProvider.NewInstanceFactory())
+
+/**单独获取[FragmentActivity]对应的[ViewModelProvider]*/
+fun FragmentActivity.of(factory: ViewModelProvider.Factory? = null): ViewModelProvider =
+    ViewModelProvider(this, factory ?: ViewModelProvider.NewInstanceFactory())
+
+//</editor-fold desc="返回ViewModelProvider">
+
+//<editor-fold desc="返回ViewModel">
+
+/**获取[ViewModelStore]中的[ViewModel], 默认的[key]是[DEFAULT_KEY + ":" + modelClass.getCanonicalName()]*/
+inline fun <reified VM : ViewModel> Fragment.vm(factory: ViewModelProvider.Factory? = null) =
+    of(factory).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> Fragment.vma(factory: ViewModelProvider.Factory? = null) =
+    ofa(factory).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> FragmentActivity.vm(factory: ViewModelProvider.Factory? = null) =
+    of(factory).get(VM::class.java)
+
 /**自定义[ViewModelStore]获取[ViewModel]*/
 inline fun <reified VM : ViewModel> ViewModelStore.vm(
     activity: Activity? = null,
     factory: ViewModelProvider.Factory? = null
 ) = of(activity, factory).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> ViewModelProvider.vm() = get(VM::class.java)
+
+//</editor-fold desc="返回ViewModel">
--- viewmodel/src/main/java/com/angcyo/viewmodel/ViewModelProperty.kt
+++ viewmodel/src/main/java/com/angcyo/viewmodel/ViewModelProperty.kt
@@ -2,6 +2,7 @@ package com.angcyo.viewmodel
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -12,14 +13,15 @@ import kotlin.reflect.KProperty
  * @date 2020/01/31
  */
 
+
 class VMAProperty<VM : ViewModel>(val vmCls: Class<VM>) : ReadOnlyProperty<Fragment, VM> {
     override fun getValue(thisRef: Fragment, property: KProperty<*>): VM {
         return thisRef.ofa().get(vmCls)
     }
 }
 
-class VMProperty<VM : ViewModel>(val vmCls: Class<VM>) : ReadOnlyProperty<Fragment, VM> {
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): VM {
+class VMProperty<VM : ViewModel>(val vmCls: Class<VM>) : ReadOnlyProperty<ViewModelStoreOwner, VM> {
+    override fun getValue(thisRef: ViewModelStoreOwner, property: KProperty<*>): VM {
         return thisRef.of().get(vmCls)
     }
 }
