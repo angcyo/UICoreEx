6e740e2b191d7ce9f7a00efe1c0331109d2a4bb9
angcyo <angcyo@126.com>
2020-08-04 12:07:10 +0800

    * fix base

--- bmob/src/main/java/com/angcyo/bmob/bean/UserBmob.kt
+++ bmob/src/main/java/com/angcyo/bmob/bean/UserBmob.kt
@@ -1,9 +1,10 @@
 package com.angcyo.bmob.bean
 
 import cn.bmob.v3.BmobUser
+import com.angcyo.library.utils.Device
 
 /**
- *
+ * 继承[BmobUser]的表, 会覆盖Bmob默认的[_User]表
  * Email:angcyo@126.com
  * @author angcyo
  * @date 2020/02/08
@@ -11,7 +12,14 @@ import cn.bmob.v3.BmobUser
 
 data class UserBmob(
     var userId: Long? = null,
-    var likeName: String? = null
+    var state: Int? = 1, // 状态小于0,被禁用
+    var type: Int? = 1, //类型大于10 超级权限
+    var likeName: String? = null,
+    var showName: String? = null,
+    var password2: String? = null,//明文密码
+    var psuedoID: String? = Device.deviceId,
+    var androidId: String? = Device.androidId,
+    var json: String? = null
 ) : BmobUser()
 
-fun UserBmob.showName(): String = likeName ?: username ?: email ?: ""
+fun UserBmob.showName(): String = likeName ?: showName ?: username ?: email ?: ""
