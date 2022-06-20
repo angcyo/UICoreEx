5fdfb27cb6c9a4f1740b1cef5e82d844258eb211
angcyo <angcyo@126.com>
2022-06-20 11:44:49 +0800

    + channel bitmap

---
+++ engrave/src/main/java/com/angcyo/engrave/EngraveKtx.kt
@@ -0,0 +1,34 @@
+package com.angcyo.engrave
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import com.angcyo.library.ex.toHexInt
+
+/**
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/06/20
+ */
+
+/**机器雕刻的色彩数据可视化*/
+fun ByteArray.toEngraveBitmap(width: Int, height: Int): Bitmap {
+    val channelBitmap =
+        Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(channelBitmap)
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        this.style = Paint.Style.FILL
+        strokeWidth = 1f
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+    val bytes = this
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            val value: Int = bytes[y * width + x].toHexInt()
+            paint.color = Color.argb(255, value, value, value)
+            canvas.drawCircle(x.toFloat(), y.toFloat(), 1f, paint)//绘制圆点
+        }
+    }
+    return channelBitmap
+}
