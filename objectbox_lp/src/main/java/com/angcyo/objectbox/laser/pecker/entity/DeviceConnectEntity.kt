f4c7f07c5c669452cf632ba0134993f8e7d8af85
angcyo <angcyo@126.com>
2022-06-29 15:34:59 +0800

    + device connect entity

---
+++ objectbox_lp/src/main/java/com/angcyo/objectbox/laser/pecker/entity/DeviceConnectEntity.kt
@@ -0,0 +1,27 @@
+package com.angcyo.objectbox.laser.pecker.entity
+
+import androidx.annotation.Keep
+import com.angcyo.library.ex.nowTime
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+
+/**
+ * 设备连接记录表
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/06/29
+ */
+
+@Entity
+@Keep
+data class DeviceConnectEntity(
+    @Id var entityId: Long = 0L,
+
+    /**设备连接的时间, 毫秒*/
+    var connectTime: Long = nowTime(),
+
+    /**设备的地址*/
+    var deviceAddress: String? = null,
+
+    /**设备的蓝牙名称*/
+    var deviceName: String? = null,
+)
