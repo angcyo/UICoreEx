c1ec597a8504965a317ed41d12bec91efb848430
angcyo <angcyo@126.com>
2022-10-12 11:31:47 +0800

    * fix base

--- tbs/src/main/java/com/angcyo/tbs/core/TbsVideoActivity.kt
+++ tbs/src/main/java/com/angcyo/tbs/core/TbsVideoActivity.kt
@@ -1,10 +1,7 @@
 package com.angcyo.tbs.core
 
-import android.content.res.Configuration
 import android.os.Bundle
-import com.angcyo.activity.showDebugInfoView
 import com.angcyo.base.dslAHelper
-import com.angcyo.library.ex.isDebug
 import com.tencent.smtt.sdk.VideoActivity
 
 /**
@@ -18,24 +15,6 @@ class TbsVideoActivity : VideoActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setBackgroundDrawable(null)
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        onShowDebugInfoView(hasFocus)
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        if (isDebug()) {
-            window.decorView.postDelayed({
-                onShowDebugInfoView()
-            }, 300)
-        }
-    }
-
-    fun onShowDebugInfoView(show: Boolean = true) {
-        showDebugInfoView(show)
     }
 
     override fun onBackPressed() {
