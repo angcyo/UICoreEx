b1ae3226791c3abce8c87fb3f04d9acf6af8f542
angcyo <angcyo@126.com>
2022-10-23 10:30:23 +0800

    + js engine

---
+++ jsEngine/README.md
@@ -0,0 +1,7 @@
+# 2022-10-23
+
+JavascriptEngine
+
+https://developer.android.com/jetpack/androidx/releases/javascriptengine
+
+https://developer.android.com/develop/ui/views/layout/webapps/jsengine
---
+++ jsEngine/src/main/java/com/angcyo/javascript/engine/JSEngine.kt
@@ -0,0 +1,15 @@
+package com.angcyo.javascript.engine
+
+/**
+ * [JavaScriptEngine]
+ *
+ * https://developer.android.com/jetpack/androidx/releases/javascriptengine
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2022/10/23
+ * Copyright (c) 2020 angcyo. All rights reserved.
+ */
+object JSEngine {
+
+}
