eaf0ebc063471b8673fc330f397fc85bf22e05cc
angcyo <angcyo@126.com>
2022-11-28 12:05:13 +0800

    + close text edit fun

--- canvas_lp/src/main/java/com/angcyo/canvas/laser/pecker/dslitem/TextStyleSelectItem.kt
+++ canvas_lp/src/main/java/com/angcyo/canvas/laser/pecker/dslitem/TextStyleSelectItem.kt
@@ -5,7 +5,9 @@ import com.angcyo.canvas.items.data.DataTextItem
 import com.angcyo.canvas.laser.pecker.R
 import com.angcyo.dialog.popup.menuPopupWindow
 import com.angcyo.dsladapter.updateItemSelected
+import com.angcyo.engrave.data.HawkEngraveKeys
 import com.angcyo.library.ex._string
+import com.angcyo.library.ex.have
 import com.hingin.umeng.UMEvent
 import com.hingin.umeng.umengEventValue
 
@@ -28,43 +30,56 @@ class TextStyleSelectItem : CanvasControlItem2() {
 
                 anchor.context.menuPopupWindow(anchor) {
                     renderAdapterAction = {
+                        val closeTextEditItemsFun = HawkEngraveKeys.closeTextEditItemsFun
                         //
-                        TextStrokeStyleItem()() {
-                            itemIco = R.drawable.canvas_text_style_solid
-                            itemText = _string(R.string.canvas_solid)
-                            itemStyle = Paint.Style.FILL
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_solid_")) {
+                            TextStrokeStyleItem()() {
+                                itemIco = R.drawable.canvas_text_style_solid
+                                itemText = _string(R.string.canvas_solid)
+                                itemStyle = Paint.Style.FILL
+                                itemRenderer = renderer
+                            }
                         }
-                        TextStrokeStyleItem()() {
-                            itemIco = R.drawable.canvas_text_style_stroke
-                            itemText = _string(R.string.canvas_hollow)
-                            itemStyle = Paint.Style.STROKE
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_stroke_")) {
+                            TextStrokeStyleItem()() {
+                                itemIco = R.drawable.canvas_text_style_stroke
+                                itemText = _string(R.string.canvas_hollow)
+                                itemStyle = Paint.Style.STROKE
+                                itemRenderer = renderer
+                            }
                         }
                         //
-                        TextStyleItem()() {
-                            itemIco = R.drawable.canvas_text_bold_style_ico
-                            itemText = _string(R.string.canvas_bold)
-                            itemStyle = DataTextItem.TEXT_STYLE_BOLD
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_bold_")) {
+                            TextStyleItem()() {
+                                itemIco = R.drawable.canvas_text_bold_style_ico
+                                itemText = _string(R.string.canvas_bold)
+                                itemStyle = DataTextItem.TEXT_STYLE_BOLD
+                                itemRenderer = renderer
+                            }
                         }
-                        TextStyleItem()() {
-                            itemIco = R.drawable.canvas_text_italic_style_ico
-                            itemText = _string(R.string.canvas_italic)
-                            itemStyle = DataTextItem.TEXT_STYLE_ITALIC
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_italic_")) {
+                            TextStyleItem()() {
+                                itemIco = R.drawable.canvas_text_italic_style_ico
+                                itemText = _string(R.string.canvas_italic)
+                                itemStyle = DataTextItem.TEXT_STYLE_ITALIC
+                                itemRenderer = renderer
+                            }
                         }
-                        TextStyleItem()() {
-                            itemIco = R.drawable.canvas_text_under_line_style_ico
-                            itemText = _string(R.string.canvas_under_line)
-                            itemStyle = DataTextItem.TEXT_STYLE_UNDER_LINE
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_underline_")) {
+                            TextStyleItem()() {
+                                itemIco = R.drawable.canvas_text_under_line_style_ico
+                                itemText = _string(R.string.canvas_under_line)
+                                itemStyle = DataTextItem.TEXT_STYLE_UNDER_LINE
+                                itemRenderer = renderer
+                            }
                         }
-                        TextStyleItem()() {
-                            itemIco = R.drawable.canvas_text_delete_line_style_ico
-                            itemText = _string(R.string.canvas_delete_line)
-                            itemStyle = DataTextItem.TEXT_STYLE_DELETE_LINE
-                            itemRenderer = renderer
+                        if (!closeTextEditItemsFun.have("_deleteline_")) {
+                            TextStyleItem()() {
+                                itemIco = R.drawable.canvas_text_delete_line_style_ico
+                                itemText = _string(R.string.canvas_delete_line)
+                                itemStyle = DataTextItem.TEXT_STYLE_DELETE_LINE
+                                itemRenderer = renderer
+                            }
                         }
                     }
                     onDismiss = {
