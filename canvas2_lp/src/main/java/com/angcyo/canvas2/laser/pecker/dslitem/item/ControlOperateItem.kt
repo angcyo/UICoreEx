61e4e03458135e4ed45093b6f93e7a7ace6d3869
angcyo <angcyo@126.com>
2023-04-15 07:59:49 +0800

    * fix operate

--- canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dslitem/item/ControlOperateItem.kt
+++ canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dslitem/item/ControlOperateItem.kt
@@ -1,8 +1,11 @@
 package com.angcyo.canvas2.laser.pecker.dslitem.item
 
 import com.angcyo.canvas2.laser.pecker.R
+import com.angcyo.canvas2.laser.pecker.dialog.arithmeticHandleDialogConfig
 import com.angcyo.canvas2.laser.pecker.dslitem.CanvasIconItem
+import com.angcyo.canvas2.laser.pecker.util.lpElement
 import com.angcyo.library.ex._string
+import com.angcyo.library.ex.size
 
 /**
  * 操作item
@@ -16,7 +19,15 @@ class ControlOperateItem : CanvasIconItem() {
         itemText = _string(R.string.canvas_operate)
         itemEnable = true
         itemClick = {
-
+            //2023-4-15 显示算法测试对话框
+            val list = itemRenderDelegate?.selectorManager?.getSelectorRendererList(true, false)
+            if (list.size() == 1) {
+                val renderer = list!!.first()
+                val element = renderer.lpElement()
+                if (element != null) {
+                    it.context.arithmeticHandleDialogConfig(element)
+                }
+            }
         }
     }
 }
