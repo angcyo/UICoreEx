5e423442854ebb9f476d91c2f8ba45cea98a36f3
angcyo <angcyo@126.com>
2023-09-06 18:08:20 +0800

    * fix base

--- canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarDateFormatInputItem.kt
+++ canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarDateFormatInputItem.kt
@@ -1,10 +1,15 @@
 package com.angcyo.canvas2.laser.pecker.dialog.dslitem
 
 import com.angcyo.item.DslSingleInputItem
+import com.angcyo.item.style.itemLabel
 
 /**
  * 自定义日期格式输入item
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @since 2023/09/06
  */
-class VarDateFormatInputItem : DslSingleInputItem()
+class VarDateFormatInputItem : DslSingleInputItem() {
+    init {
+        itemLabel = ""
+    }
+}
--- canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarTimeFormatInputItem.kt
+++ canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarTimeFormatInputItem.kt
@@ -1,10 +1,15 @@
 package com.angcyo.canvas2.laser.pecker.dialog.dslitem
 
 import com.angcyo.item.DslSingleInputItem
+import com.angcyo.item.style.itemLabel
 
 /**
  * 自定义时间格式输入item
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @since 2023/09/06
  */
-class VarTimeFormatInputItem : DslSingleInputItem()
+class VarTimeFormatInputItem : DslSingleInputItem() {
+    init {
+        itemLabel = ""
+    }
+}
