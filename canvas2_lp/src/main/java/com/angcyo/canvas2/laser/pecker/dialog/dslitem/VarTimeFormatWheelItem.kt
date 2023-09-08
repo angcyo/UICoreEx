4b4dd0bfe17bd6e3a3ece60c1ccd5fc3bdb27bfc
angcyo <angcyo@126.com>
2023-09-08 10:41:44 +0800

    * fix date/time format preview

--- canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarTimeFormatWheelItem.kt
+++ canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/VarTimeFormatWheelItem.kt
@@ -3,7 +3,10 @@ package com.angcyo.canvas2.laser.pecker.dialog.dslitem
 import com.angcyo.bluetooth.fsc.laserpacker._deviceSettingBean
 import com.angcyo.dialog2.dslitem.LPLabelWheelItem
 import com.angcyo.dialog2.dslitem.itemWheelList
+import com.angcyo.dialog2.dslitem.itemWheelToTextAction
 import com.angcyo.item.style.itemLabel
+import com.angcyo.library.component.parser.parseDateTemplate
+import com.angcyo.library.ex.toStr
 
 /**
  * 预设的时间格式选择item
@@ -15,6 +18,10 @@ class VarTimeFormatWheelItem : LPLabelWheelItem() {
     init {
         itemWheelList = _deviceSettingBean?.timeFormatList
         itemLabel = ""
+
+        itemWheelToTextAction = {
+            it.toStr().parseDateTemplate()
+        }
     }
 
 }
