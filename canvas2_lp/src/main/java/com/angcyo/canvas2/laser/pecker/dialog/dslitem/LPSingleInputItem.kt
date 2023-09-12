0f91e524bb6c228146b42dd3eb5f98fed4f5f1c7
angcyo <angcyo@126.com>
2023-09-12 18:28:24 +0800

    * fix style

--- canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/LPSingleInputItem.kt
+++ canvas2_lp/src/main/java/com/angcyo/canvas2/laser/pecker/dialog/dslitem/LPSingleInputItem.kt
@@ -2,13 +2,21 @@ package com.angcyo.canvas2.laser.pecker.dialog.dslitem
 
 import com.angcyo.canvas2.laser.pecker.R
 import com.angcyo.item.DslSingleInputItem
+import com.angcyo.item.style.itemEditMaxInputLength
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @since 2023/09/12
  */
 open class LPSingleInputItem : DslSingleInputItem() {
+
+    companion object {
+        /**允许默认输入的字符长度*/
+        var LP_DEFAULT_MAX_INPUT_LENGTH = 20
+    }
+
     init {
         itemLayoutId = R.layout.lp_single_input_item
+        itemEditMaxInputLength = LP_DEFAULT_MAX_INPUT_LENGTH
     }
 }
